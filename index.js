efa95ad4439d70b72bf44b13db2021cbe881a4b9
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-01-09 15:07:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 
-let name1 = prompt("What's your name?");
-let name2 = prompt("What is their name?");
-
 let loveScore = Math.random();
 loveScore = Math.floor((loveScore* 100)+1);
 
-
-
-document.querySelector(".btn").addEventListener("click", function(){
+document.querySelector(".btn").addEventListener("click", function(){    
+    let name1 = prompt("What's your name?");
+    let name2 = prompt("What is their name?");
     document.querySelector("h1").innerHTML = name1 + " and "+ name2 +" love score is "+loveScore+ "%"
 })
